3e302842b337938945650940c81181cc4c2d3685
Maxym_Tomusiak <101803422+Maksym-Tomusiak@users.noreply.github.com>
2024-04-01 14:01:27 +0300

    feat: add project

---
+++ Lab4-5/Lab4/HalfPlane.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class HalfPlane
+    {
+        protected double[] coefficients;
+        public HalfPlane() : this(0, 0, 0)
+        { }
+        public HalfPlane(double a1, double a2, double b)
+        {
+            coefficients = new double[] { a1, a2, b };
+        }
+
+        public void SetCoefficients(double a1, double a2, double b)
+        {
+            coefficients[0] = a1;
+            coefficients[1] = a2;
+            coefficients[2] = b;
+        }
+        public virtual void GetCoeficientFromConsole()
+        {
+            Console.WriteLine("Write coeficients a1, a2, b:");
+            List<double> input = Console.ReadLine().Split(' ').Select(c => double.Parse(c)).ToList();
+            while (input.Count < 3)
+            {
+                input.Add(0);
+            }
+            coefficients[0] = input[0];
+            coefficients[1] = input[1];
+            coefficients[2] = input[2];
+        }
+
+        public virtual void DisplayCoefficients()
+        {
+            Console.WriteLine($"Coefficients: {coefficients[0]}, {coefficients[1]}, {coefficients[2]}");
+        }
+
+        public virtual bool IsPointInside()
+        {
+            Console.WriteLine("Enter point coordinates x,y:");
+            double[] x = Console.ReadLine().Split(' ').Select(cor => double.Parse(cor)).ToArray();
+            return x[0] * coefficients[0] + x[1] * coefficients[1] < coefficients[2];
+        }
+    }
+}
---
+++ Lab4-5/Lab4/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System.Runtime.CompilerServices;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string choice;
+            do
+            {
+                Console.WriteLine("Enter what to create(half-plane, half-space):");
+                choice = Console.ReadLine();
+            } while (choice != "half-plane" && choice != "half-space");
+            HalfPlane curFigure = null;
+            switch (choice)
+            {
+                case "half-plane":
+                    curFigure = new HalfPlane();
+                    break;
+                case "half-space":
+                    curFigure = new HalfSpace();
+                    break;
+            }
+            curFigure.GetCoeficientFromConsole();
+            if (curFigure.IsPointInside())
+            {
+                Console.WriteLine("Your point is inside");
+            }
+            else
+            {
+                Console.WriteLine("Your point is not inside");
+            }
+        }
+    }
+}
